453f3149f24b7c1e6e872be1886dbd77e1bb67a3
Jose Segura <jossegura2023@u.northwestern.edu>
2023-04-11 23:06:36 -0500

    modified the index.js to suite the class method

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fs = require('fs'); // imports the fs package needed to create the svg file
-
-const { generateSVG } = require('./logo-generator'); // imports the code that will generate the shape, color, and text according to the prompts
+const inquirer = require('inquirer');
+const { LogoGenerator } = require('./logo-generator.js'); // imports the code that will generate the shape, color, and text according to the prompts
 
 //
 async function main() {
-  const inquirer = await import('inquirer');
 // prompt questions the user will be asked before the logo is generated
     const questions = [
       {
@@ -32,8 +31,9 @@ async function main() {
     ];
 
     // Here we correlate the answers given with the questions and those answers will be used the generate the svg 
-    const answers = await inquirer.default.prompt(questions);
-    const svg = generateSVG(answers);
+    const answers = await inquirer.prompt(questions);
+    const logoGenerator = new LogoGenerator(200, 200);
+    const svg = logoGenerator.generateSVG(answers);
 // This line of code creates the file under svg and as well the file name such as pollo.svg
     fs.writeFileSync(`${answers.filename}.svg`, svg);
     console.log(`SVG file has been saved as ${answers.filename}.svg`);  
