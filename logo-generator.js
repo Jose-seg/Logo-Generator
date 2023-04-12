3843d889ea1dc0551cece5232a4f523a62b4dcb1
Jose Segura <jossegura2023@u.northwestern.edu>
2023-04-11 22:50:14 -0500

    modified my logo-generator.js file to use class method

--- logo-generator.js
+++ logo-generator.js
@@ -1,37 +1,29 @@
-// imports the packages we will use for this backend app
-const { SVG, registerWindow } = require('@svgdotjs/svg.js');
-const {createSVGWindow} = require('svgdom');
-// function for generating the svg according to the user's selected options
-function generateSVG(options) {
-    const {color, shape, text} = options;
-    const width = 200;
-    const height = 200;
+const { Circle, Square, Triangle } = require('./shapes.js');
 
-// used different packages such as svgdom and svgdotjs to be able to make the svg file
-    const window = createSVGWindow();
-    const document = window.document;
-    registerWindow(window, document);
-
-    const canvas = SVG(document.documentElement).size(width, height);
-// if statements for the choices in shape the user may choose
-    if (shape === 'circle') {
-        canvas.circle(width).fill(color).center(width/ 2, height / 2); 
-    } else if (shape === 'square') {
-        canvas.rect(width, height).fill(color);
-    } else if (shape === 'triangle') {
-        canvas.polygon([
-            [width/ 2, 0],
-            [width, height],
-            [0, height],
-        ]).fill(color);
+class LogoGenerator {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
     }
-   // Code for putting the text onto the canvas 
-    canvas.text(text)
-    .font({family: 'Arial', size: 24, anchor: 'middle', leading: '1.5em'})
-    .fill('white')
-    .move(width / 2, height / 2 - 12);
 
-    return canvas.svg();
+    generateSVG(options) {
+        const { color, shape, text } = options;
+        let shapeObject;
+
+        if (shape === 'circle') {
+            shapeObject = new Circle(this.width / 2, color);
+        } else if (shape === 'square') {
+            shapeObject = new Square(this.width, color);
+        } else if (shape === 'triangle') {
+            shapeObject = new Triangle(this.width, color);
+        }
+
+        return `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${this.width} ${this.height}">
+            ${shapeObject.render()}
+            <text x="${this.width / 2}" y="${this.height / 2}" text-anchor="middle" font-family="Arial" font-size="24" fill="white">${text}</text>
+            </svg>`;
+    }
 }
-// here we export the generate svg function to import it to the index.mjs
-module.exports = { generateSVG };
+
+module.exports = {LogoGenerator};
--- tests/logoGenerator.test.js
+++ tests/logoGenerator.test.js
@@ -1,54 +1,17 @@
-const { generateSVG } = require('../logo-generator');
-// Here we start with the first test which is for the circle option and its properties
-describe('generateSVG', () => {
-    test('generates a valid SVG with the given options', () => {
+const { LogoGenerator } = require('../logo-generator');
+
+describe('LogoGenerator', () => {
+    const logoGenerator = new LogoGenerator(200, 200);
+
+    test('generateSVG generates a valid SVG with a circle shape', () => {
         const options = {
             color: 'red',
             shape: 'circle',
             text: 'Circle Logo',
         };
 
-        const svg = generateSVG(options);
-       // Here we let the test know what we should be expecting in order to pass
-        expect(svg).toContain('<svg');
-        expect(svg).toContain('</svg>');
-        // here we expect to return to color of the shape and properties that was chosen
-        expect(svg).toContain(`<circle r="100" cx="100" cy="100" fill="${options.color}"></circle>`);
-        // This code will search for the text content thats inside the tspan element, which is nested inside of  the <text> element
-        expect(svg).toMatch(/<text[^>]*>[^<]*<tspan[^>]*>[^<]*Circle Logo[^<]*<\/tspan>[^<]*<\/text>/)
+        const svg = logoGenerator.generateSVG(options);
+        expect(svg).toContain
     });
-// this generates our second test for the square shape
-    test('generates a valid SVG with a square shape', () => {
-        const options = {
-            color: 'blue',
-            shape: 'square',
-            text: 'Square Logo',
-        };
 
-        const svg = generateSVG(options);
-
-        expect(svg).toContain('<svg');
-        expect(svg).toContain('</svg>');
-         // here we expect to return to color and properties of the shape that was chosen
-        expect(svg).toContain(`<rect width="200" height="200" fill="${options.color}"></rect>`)
-         // This code will search for the text content thats inside the tspan element, which is nested inside of  the <text> element
-        expect(svg).toMatch(/<text[^>]*>[^<]*<tspan[^>]*>[^<]*Square Logo[^<]*<\/tspan>[^<]*<\/text>/)
-    });
-// this generates our second test for the triangle shape
-    test('generates a valid SVG with a triangle shape', () => {
-        const options = {
-            color: 'green',
-            shape: 'triangle',
-            text: 'Triangle Logo',
-        };
-
-        const svg = generateSVG(options);
-
-        expect(svg).toContain('<svg');
-        expect(svg).toContain('</svg>');
-         // here we expect to return to color of the shape and properties that was chosen
-        expect(svg).toContain(`<polygon points="100,0 200,200 0,200" fill="${options.color}"></polygon>`);
-         // This code will search for the text content thats inside the tspan element, which is nested inside of  the <text> element
-        expect(svg).toMatch(/<text[^>]*>[^<]*<tspan[^>]*>[^<]*Triangle Logo[^<]*<\/tspan>[^<]*<\/text>/)
-    });
 });
